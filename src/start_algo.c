2e8dffbe417cb8bbe9be5722d6dd598a8a89c892
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-04-29 11:50:21 -0400

    Update: fixe send ptr &p[x]

--- src/start_algo.c
+++ src/start_algo.c
@@ -5,29 +5,28 @@ void *ft_pthread(void *arg)
 	t_state_philo *p;
 	
 	p = ((t_state_philo *)arg);
-	printf("Hola soy el Hilo No: %d\n", p->[0]->id);
-	printf("DIR p : %p\n", p);
+	printf("Hola soy el Hilo No: %d\n", p->id);
+	p->born = milli();
+	printf("Naci hace: %ld\n", p->born);
 	return (NULL);
 }
 
 void start_philos(t_args *args)
 {
 	int	i;
-	int j;
 	t_args *a;
 	t_state_philo *p;
 
 	i = 0;
-	j = 1;
 	a = args;
+	printf("Acceso ARGS directo No phi %d\n", args->phi);
 	p = malloc(sizeof(t_state_philo) * a->phi);
 	if (!p)
 		return ;
 	while (i < a->phi)
 	{
-		p[i].id = i;
-		pthread_create(&p[i].thread_id, NULL, ft_pthread, &p);
-		p->born = milli();
+		p[i].id = i + 1;
+		pthread_create(&p[i].thread_id, NULL, ft_pthread, &p[i]);
 		i++;
 	}
 	i = 0;
@@ -36,5 +35,6 @@ void start_philos(t_args *args)
 		pthread_join(p[i].thread_id, NULL);
 		i++;
 	}
-	printf("DIR p PRINCIPAL : %p\n", &p);
+	sleep(1);
+	printf("Tiempo transcurrido desde el inicio: %ld\n", milli() - p[0].born);
 }
